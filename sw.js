66a04b4d1c63933d0c1a6717596747e1d5b3cbee
Vasu-Sehgal <62380004+Vasu-Sehgal@users.noreply.github.com>
2026-09-02 03:59:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'babylog-v1';
+const CACHE = 'babylog-v2';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
